d62a1bc1427424ac0727428fe83738d7c2f53602
Tecnicamente <lippoassoluto@yahoo.it>
2016-03-05 23:26:55 +0100

    Generator of random data set

--- GenerateRndData.py
+++ GenerateRndData.py
@@ -5,13 +5,16 @@ Created on Mar  05 2016
 @author: Tecnicamente
 This module will contains functions for generate random data to test
 the format is CSV
+
+29/03/2016
+Version 1.0
 """
 import numpy as np
 import csv
 from myfile import CreaFileOutput
 
 
-NumberOfItemToGenerate		= 300
+AvailableWorkingDays		= 220	# available working days in one year
 CostPerHour					= 80 	# €/h
 
 RevenueFactor				= 0.2	# %
@@ -30,22 +33,41 @@ MaximimTurnoverPerMachine	= 30000	# €
 MinimumTimeForDevelopment	= 150	# h
 MaximumTimeForDevelopment	= 500	# h
 
+MaximumProjectsRequests		= 20	# Projects request per year
+
 OutputFile = "ListOfProjects.csv"
 OutputFile = CreaFileOutput(OutputFile)
 
-FirstRow = "Request Number","Development Time (h)","Development costs (€)","Machines,Turnover (€)","Revenue (€)"
+FirstRow = "Request Number","Day when required","Development Time (h)","Development costs (€)","Machines","Turnover (€)","Revenue (€)"
 
 writer = csv.writer(OutputFile)
+writer.writerow(FirstRow)
 
-for i in range(NumberOfItemToGenerate):
+ListOfRows = []
+
+# radom data set creation
+for i in range(MaximumProjectsRequests):
 	row=[]
-	row.append(i)																					# 0
-	row.append(np.random.randint(MinimumTimeForDevelopment,MaximumTimeForDevelopment))				# 1
-	row.append(row[1]*CostPerHour)																	# 2
-	row.append(np.random.randint(MinimunNumberOfMachines,MaximumNumberOfMachines))					# 3
-	row.append(row[3] * np.random.randint(MinimumTurnoverPerMachine,MaximimTurnoverPerMachine))		# 4
-	row.append(row[4] * RevenueFactor * (np.random.randint(0,PertubationForRevenueFactor * 100)/100.))# 5
-	writer.writerow(row)
+	row.append(i)																						# 0	Generate the item number
+	row.append(np.random.randint(0,AvailableWorkingDays))												# 1	Generate the day of the year when the project request is communicated
+	row.append(np.random.randint(MinimumTimeForDevelopment,MaximumTimeForDevelopment))					# 2 Generate the required time for the development
+	row.append(row[2]*CostPerHour)																		# 3
+	row.append(np.random.randint(MinimunNumberOfMachines,MaximumNumberOfMachines))						# 4
+	row.append(row[4] * np.random.randint(MinimumTurnoverPerMachine,MaximimTurnoverPerMachine))			# 5
+	row.append(row[5] * RevenueFactor * (np.random.randint(0,PertubationForRevenueFactor * 100)/100.))	# 6
+	ListOfRows.append(row)
+
+# sort of random data set in function of the value "Day when required"
+buffer = []
+for i in range(MaximumProjectsRequests):
+	for j in range(i,MaximumProjectsRequests - 1):
+		if ListOfRows[i][1] > ListOfRows[j][1]:
+			buffer = ListOfRows[i]
+			ListOfRows[i] = ListOfRows[j]
+			ListOfRows[j] = buffer	
+
+for i in range(MaximumProjectsRequests):
+	writer.writerow(ListOfRows[i])
 
 OutputFile.close
 	
